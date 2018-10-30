eba4c17ce92c47a8fcf6faefc87cab6fa4516249
Vyankat Sattarwar <vyankat.sattarwar@tomtom.com>
2018-10-30 19:02:40 +0530

    added raster maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare let L;
+declare let tomtom: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const map = tomtom.L.map('map', {
+      key: '<api-key>',
+      basePath: 'sdk',
+      center: [ 52.360306, 4.876935 ],
+      zoom: 15,
+      source : 'raster'
+    });
+  }
   title = 'TTMaps';
 }
